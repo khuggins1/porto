12b24f920fea446603aebe70675646da480b2a38
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-20 17:56:30 -0800

    navbarfix

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import { About, Footer, Header, Skills, Testimonial, Work} from './container';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+
+import { Navbar } from './components';
+
 
 const App = () => {
   return (
-    <div className= "App">
+    <div className= "app">
+      <Navbar />
       <Header />
       <About />
       <Work />
