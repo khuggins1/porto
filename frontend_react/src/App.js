ca1783338a7e2e065fc83e08a07e6111637be062
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-20 18:59:59 -0800

    appcss

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -5,6 +5,8 @@ import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
 
 
+import './App.scss';
+
 const App = () => {
   return (
     <div className= "app">
