c9f783d1d0576feffb6d37de2f0ec16386d91da6
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-22 22:15:38 -0800

    headerstyles

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -6,9 +6,20 @@ import { images } from '../../constants';
 
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
       whileInView={{ x: [-100, 0 ], opacity: [0, 1]}}
       //slide in motion
@@ -45,13 +56,24 @@ const Header = () => {
         src={images.circle}
         alt="profile_circle"
         className="overlay_circle"
+        />
+
+        </motion.div>
+        
+        <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
         >
-
-        </motion.img>
-
+          {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
       </motion.div>
       </div>
   )
-}
+          }
+          
 
 export default Header
