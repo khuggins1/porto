5340d9cb28a191dc2aafd7d0af336c6e6b13f210
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-23 21:25:29 -0800

    fixaboutpix

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -9,10 +9,10 @@ import './About.scss';
 
 const abouts =  
 [
-  {title: 'Web Development', description: 'I am a good web developer.', imgUrl: ''},
-  {title: 'Web Design', description: 'I am a good web developer.', imgUrl: ''},
-  {title: 'UX/UI', description: 'I am a good web developer.', imgUrl: ''},
-  {title: 'Web Animations', description: 'I am a good web developer.', imgUrl: ''},
+  {title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
+  {title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
+  {title: 'UX/UI', description: 'I am a good web developer.', imgUrl: images.about03},
+  {title: 'Web Animations', description: 'I am a good web developer.', imgUrl: images.about04},
 ];
 
 const About = () => {
@@ -30,7 +30,7 @@ const About = () => {
           className="app__profile-item"
           key={about.title + index}
         >
-          <img src={urlFor(about.imgUrl)} alt={about.title} />
+          <img src={about.imgUrl} alt={about.title} />
           <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
           <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
         </motion.div>
