7c36e768e16ea8798618e167c3f85605d9a8f142
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-23 21:46:56 -0800

    link sanity backend

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -10,9 +10,9 @@ import './About.scss';
 const abouts =  
 [
   {title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
-  {title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
-  {title: 'UX/UI', description: 'I am a good web developer.', imgUrl: images.about03},
-  {title: 'Web Animations', description: 'I am a good web developer.', imgUrl: images.about04},
+  {title: 'Frontend Development', description: 'I am a good web developer.', imgUrl: images.about02},
+  {title: 'Backend Development', description: 'I am a good web developer.', imgUrl: images.about03},
+  {title: 'MERN Stack', description: 'I am a good web developer.', imgUrl: images.about04},
 ];
 
 const About = () => {
