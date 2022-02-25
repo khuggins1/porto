b02a4d15114e6d61964e8fe7b88623e448697b79
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-24 18:47:03 -0800

    backenddevelopment

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -6,16 +6,20 @@ import { images } from '../../constants';
 
 import './About.scss';
 
+import { urlFor, client } from '../../client';
 
-const abouts =  
-[
-  {title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
-  {title: 'Frontend Development', description: 'I am a good web developer.', imgUrl: images.about02},
-  {title: 'Backend Development', description: 'I am a good web developer.', imgUrl: images.about03},
-  {title: 'MERN Stack', description: 'I am a good web developer.', imgUrl: images.about04},
-];
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
@@ -30,7 +34,7 @@ const About = () => {
           className="app__profile-item"
           key={about.title + index}
         >
-          <img src={about.imgUrl} alt={about.title} />
+          <img src={urlFor(about.imgUrl)} alt={about.title} />
           <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
           <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
         </motion.div>
