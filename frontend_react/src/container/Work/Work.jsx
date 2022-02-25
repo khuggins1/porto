a538f2fc9ddf547dbe82b9393c2aa1e254b663a6
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-24 21:45:00 -0800

    workstyles

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -85,12 +85,16 @@ const Work = () => {
               <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
               </div>
               </div>
          ))}
         </motion.div>
   </>
-  )
-}
+  );
+};
 
-export default Work
+export default AppWrap(Work, 'work');
