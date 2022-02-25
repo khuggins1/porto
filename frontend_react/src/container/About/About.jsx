02a9b8639260e4b4822dff133acd1400163e16f8
Kassim <kassimhuggins@MacBook-Pro.lan>
2022-02-24 21:25:13 -0800

    wrapperetc

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,9 +2,10 @@ import React, {useState, useEffect } from 'react';
 
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
 
 import './About.scss';
+
+import { AppWrap } from '../../wrapper';
 
 import { urlFor, client } from '../../client';
 
@@ -44,4 +45,4 @@ const About = () => {
   );
 };
 
-export default About
+export default AppWrap(About, 'about');
